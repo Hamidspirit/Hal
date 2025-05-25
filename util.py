bceb18601fa3f78e36dc39e32d92c20c70f8cab9
Spirit <hamidrezacheghini578@gmail.com>
2025-05-25 20:30:53 +0330

    add handwritten image function

--- util.py
+++ util.py
@@ -1,12 +1,23 @@
+import scipy.misc
+import matplotlib.pyplot as plt
 import numpy
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+
 
 def res_image(value):
     """Return an image of the value. it takes a list"""
 
-    image_array = numpy.asarray(value[1:], dtype=numpy.float32).reshape((28,28))
+    image_array = numpy.asarray(
+        value[1:], dtype=numpy.float32).reshape((28, 28))
     plt.imshow(image_array, cmap="Grays", interpolation='None')
     plt.savefig("output.png")
 
+
+def hand_written(image_name):
+    """Return a handwritten image data to query network"""
+    img_array = scipy.misc.imread(image_name, flatten=True)
+
+    image_data = 255.0 - img_array.reshape(784)
+    image_data = (image_data / 255.0 * 0.99) + 0.01
+    return image_data
